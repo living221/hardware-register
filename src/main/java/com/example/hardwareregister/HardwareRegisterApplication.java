2facde7e7c6e8aaae2f1da47f895b43da6e53dc5
living221 <living221@gmail.com>
2023-05-31 11:41:25 +0700

    feat: start commit

---
+++ src/main/java/com/example/hardwareregister/HardwareRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.hardwareregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwareRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hardwareregister/HardwareRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hardwareregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
